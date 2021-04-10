847adec80d8f79f2497e8f9979e3944dd05f02ed
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-09 20:15:10 -0500

    task done

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassesAndObjectsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Books book1 = new Books();
+            book1.title = "Love Me";
+            book1.author = "Koko Padgett";
+            book1.pages = 389;
+
+
+            Books book2 = new Books();
+            book2.title = "Ring Me";
+            book2.author = "Tony Padgett";
+            book2.pages = 217;
+
+
+            Console.WriteLine(book1.title);
+            
+           
+            
+
+
+
+        }
+    }
+}
